b07888b6bd0d3386e9943b3232676a558c305541
Batuhan-Durhan <71403385+Batuhan-Durhan@users.noreply.github.com>
2022-03-30 02:00:54 +0000

    index.htmnl

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 Batuhan Durhan All rights reserved
+//
+// Created by: Batuhan Durhan
+// Created on: Batuhan Durhan
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function enterClicked() {
+  // input
+  const hours = parseInt(document.getElementById('numbers-of-hours').value)
+  const rate = parseInt(document.getElementById('hourly-rate').value)
+  parseFloat("10.33")
+
+  // process
+  const pay = (rate * hours) * (1.00-0.18)
+  const taxes = (rate * hours) * 0.18
+
+  // output
+  document.getElementById('pay').innerHTML = 'Pay is: ' + pay + ' $'
+  document.getElementById('taxes').innerHTML = 'Taxes is: ' + taxes + ' $'
+}
